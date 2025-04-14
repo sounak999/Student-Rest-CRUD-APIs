75bd9786688d175fe428f1e0d8c6e3a5fed8f0f0
Sounak <sounaksaha333@gmail.com>
2025-04-15 00:20:47 +0530

    adding exception handler

--- rest-crud/src/main/java/org/student/api/rest_crud/controller/StudentApiController.java
+++ rest-crud/src/main/java/org/student/api/rest_crud/controller/StudentApiController.java
@@ -1,11 +1,12 @@
 package org.student.api.rest_crud.controller;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.student.api.rest_crud.entity.Student;
+import org.student.api.rest_crud.error.StudentErrorResponse;
+import org.student.api.rest_crud.exception.StudentNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +31,21 @@ public class StudentApiController {
 
     @GetMapping("/student/{studentId}")
     public Student getStudent(@PathVariable int studentId) {
+        if (studentId < 0 || studentId >= studentList.size()) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
         return studentList.get(studentId);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
+        StudentErrorResponse error = new StudentErrorResponse();
+
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+        error.setMessage(ex.getMessage());
+        error.setTimestamp(System.currentTimeMillis());
+
+        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
     }
 }

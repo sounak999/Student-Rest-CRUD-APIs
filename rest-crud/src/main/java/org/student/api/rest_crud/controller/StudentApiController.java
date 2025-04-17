de4e12133ca2efaa7f28b88cdc487035129f2a42
Sounak <sounaksaha333@gmail.com>
2025-04-18 02:36:40 +0530

    global exception handling implemented

--- rest-crud/src/main/java/org/student/api/rest_crud/controller/StudentApiController.java
+++ rest-crud/src/main/java/org/student/api/rest_crud/controller/StudentApiController.java
@@ -1,11 +1,8 @@
 package org.student.api.rest_crud.controller;
 
 import jakarta.annotation.PostConstruct;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.student.api.rest_crud.entity.Student;
-import org.student.api.rest_crud.error.StudentErrorResponse;
 import org.student.api.rest_crud.exception.StudentNotFoundException;
 
 import java.util.ArrayList;
@@ -38,25 +35,4 @@ public class StudentApiController {
         return studentList.get(studentId);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException ex) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.NOT_FOUND.value());
-        error.setMessage(ex.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<StudentErrorResponse> handleException(Exception ex) {
-        StudentErrorResponse error = new StudentErrorResponse();
-
-        error.setStatus(HttpStatus.BAD_REQUEST.value());
-        error.setMessage(ex.getMessage());
-        error.setTimestamp(System.currentTimeMillis());
-
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
-    }
 }
